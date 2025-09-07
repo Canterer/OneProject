256d410abb5a43fe3939a797ae8ced18f64dc600
Canterer <1170836331@qq.com>
2025-09-08 00:39:22 +0800

    update code

--- Source/new_script.gd
+++ Source/new_script.gd
@@ -1,1 +1,10 @@
 extends Node
+
+func _ready() -> void:
+	var client = StreamPeerTCP.new()
+	client.connect_to_host("127.0.0.1", 8001)
+	print("client_tcp _ready")
+	if client.get_status() == StreamPeerTCP.Status.STATUS_CONNECTED:
+		client.put_string("hallo server")
+	else:
+		print("client connect error!!",client.get_status())
